e23bc51a6edd4650ffbe6bef5e082f9916b70935
MollieS <mstephenson@8thlight.com>
2016-11-29 13:45:56 +0000

    Adds views

--- src/main/java/tttweb/controllers/GameController.java
+++ src/main/java/tttweb/controllers/GameController.java
@@ -4,22 +4,53 @@ import httpserver.Request;
 import httpserver.Response;
 import httpserver.httprequests.RequestHeader;
 import httpserver.httpresponse.HTTPResponse;
+import httpserver.httpresponse.ResponseHeader;
 import httpserver.routing.Route;
+import httpserver.sessions.Session;
+import httpserver.sessions.SessionFactory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static httpserver.httpresponse.StatusCode.OK;
 import static httpserver.httpresponse.StatusCode.REDIRECT;
 import static httpserver.routing.Method.POST;
 
-public class NewGameController extends Route {
+public class GameController extends Route {
 
-    public NewGameController() {
+    private final SessionFactory sessionFactory;
+    private List<Session> sessions = new ArrayList<>();
+
+    public GameController(SessionFactory sessionFactory) {
         super("/game", POST);
+        this.sessionFactory = sessionFactory;
     }
 
     @Override
     public Response performAction(Request request) {
-        String gameType = (request.getValue(RequestHeader.DATA));
-        System.out.println(gameType);
+        if (!request.hasHeader(RequestHeader.COOKIE)) {
+            return redirect();
+        }
+        String sessionID = request.getValue(RequestHeader.COOKIE);
+        if (isNewSession(sessionID)) {
+            sessions.add(sessionFactory.createSession(sessionID));
+        }
+        return HTTPResponse.create(OK);
+    }
 
-        return HTTPResponse.create(REDIRECT);
+    private Response redirect() {
+        HashMap<ResponseHeader, byte[]> headers = new HashMap<>();
+        headers.put(ResponseHeader.LOCATION, "/".getBytes());
+        return HTTPResponse.create(REDIRECT).withHeaders(headers);
+    }
+
+    public boolean isNewSession(String sessionID) {
+        for (Session session : sessions) {
+            if (session.getId().equals(sessionID)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
